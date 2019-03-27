fe331ed19801de50cbeea8bf9c4c457347f8c850
alyssapacleb <alyssapacleb@gmail.com>
2019-03-27 10:09:01 -0500

    Can delete table rows, not consistent with core data tho

--- group21_assignment6/AdventurersTableViewController.swift
+++ group21_assignment6/AdventurersTableViewController.swift
@@ -20,7 +20,7 @@ class AdventurersTableViewController: UITableViewController {
         print(Adventurers)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
@@ -88,17 +88,19 @@ class AdventurersTableViewController: UITableViewController {
         return true
     }*/
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            Adventurers.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            print(Adventurers)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
